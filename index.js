bef0859f32ba6ea00256b1932b14a99a502618fa
john-reiner <johnreiner1990@gmail.com>
2020-08-04 17:31:00 -0400

    rendered buttons to calc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('somthing')
+window.addEventListener('DOMContentLoaded', (event) => {
+    const buttonContainer = document.getElementById('buttons')
+
+
+    const calculator = {
+        buttons: ['ac','+/-', '%', '÷', 7,8,9,'x',4,5,6,'-',1,2,3,'+',0,'.','='],
+        
+    }
+    const renderButtons = () => {
+        
+        calculator.buttons.map(button => {
+            div = document.createElement('div')
+            div.className = 'button'
+            div.innerText = button
+            console.log(div)
+            buttonContainer.appendChild(div)
+        })
+    }
+
+    let display = []
+
+    buttonContainer.addEventListener('click', (event) => {
+        if (event.target.className === 'button') {
+            let button = event.target.innerText
+            
+            let displayToShow = display.push(button) 
+            console.log(display, button, displayToShow)
+        }
+    })
+
+    renderButtons()
+})
