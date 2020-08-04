03f269b209d8f3a196a00e68d5d770ffefa317c0
john-reiner <johnreiner1990@gmail.com>
2020-08-04 18:26:43 -0400

    working on calcuate function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const buttonContainer = document.getElementById('buttons')
-
+    const display = document.getElementById('display')
 
     const calculator = {
         buttons: ['ac','+/-', '%', '÷', 7,8,9,'x',4,5,6,'-',1,2,3,'+',0,'.','='],
@@ -12,19 +12,41 @@ window.addEventListener('DOMContentLoaded', (event) => {
             div = document.createElement('div')
             div.className = 'button'
             div.innerText = button
-            console.log(div)
             buttonContainer.appendChild(div)
         })
     }
 
-    let display = []
+    const calculate = () => {
+        let number = 
+        displayArray.forEach(value => {
+
+            if (value === '+/-' || value === '%' || value === '÷' || value === 'x' || value === '-' || value === '+') {
+                console.log('found: ', value)
+            } 
+        })
+    }
+
+
+
+    let displayArray = []
+
+    const updateDisplay = () => {
+        let displayToShow = displayArray.join('')
+        display.innerText = displayToShow
+    }
 
     buttonContainer.addEventListener('click', (event) => {
         if (event.target.className === 'button') {
             let button = event.target.innerText
-            
-            let displayToShow = display.push(button) 
-            console.log(display, button, displayToShow)
+            if (button === '=') {
+                calculate()
+                
+            } else if (button !== 'ac') {
+                displayArray.push(button)
+                updateDisplay()             
+            } else {
+                displayArray = []
+            }
         }
     })
 
